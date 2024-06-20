e9cafc43939b2e22e9c3dadd2f4a0d302d22f925
DUHO3ABP52 <biryukov_denis@rambler.ru>
2024-06-20 13:23:58 +0300

    v1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   assetsDir: 'assets',
+  build: {
+    outDir: '../dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
 
